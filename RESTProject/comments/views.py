f798802dff2e30c54ed63c916d9163ec2ccc1ea7
rzhvn1 <erzhanmuratov99@gmail.com>
2021-04-05 18:10:58 +0600

    views file to add new logic in comments app

---
+++ RESTProject/comments/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import views, status
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+# Create your views here.
+
+
+
+
